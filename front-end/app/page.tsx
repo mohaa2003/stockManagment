790c70902f59caca9ab2c1c95f610242eaf81067
mohamedilyes <mohamedilyes.bouguerri@gmail.com>
2024-07-21 20:23:09 +0100

    adding font to project

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -2,15 +2,10 @@ import Aside from "./components/aside/aside";
 import Section from "./components/section/section";
 import { Montserrat } from 'next/font/google';
 
-const inter = Montserrat({ 
-  subsets: ['latin'], 
-  weight: ['400', '700'], 
-  style: ['normal', 'italic'] 
-});
 
 export default function Home() {
   return(
-        <main className="h-screen grid rounded-lg">
+        <main className=" h-screen grid rounded-lg">
             <Aside/>
             <Section/>
         </main>
