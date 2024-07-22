b7ea44d4a6b4a95c3bd971bc6b4e42fa6b624e09
mohamedilyes <mohamedilyes.bouguerri@gmail.com>
2024-07-22 16:34:22 +0100

    to personal profile link setting

--- front-end/app/components/nav/nav.tsx
+++ front-end/app/components/nav/nav.tsx
@@ -1,12 +1,17 @@
+import Link from "next/link";
+import Image from "next/image";
 import "./nav.css";
+import profilPic from "@/public/profil_pic.png";
 const Nav : React.FC = () => {
     return(
-            <nav className="flex justify-between navbar mx-auto bg-white h-20 w-full rounded-2xl items-center px-4">
+            <nav className="flex justify-between navbar mx-auto bg-white h-20 w-full rounded-2xl items-center px-8">
                 <div className="title">
-                    <h2>Les Achats</h2>
+                    <h2 className="text-lg font-bold">Les Achats</h2>
                 </div>
                 <div className="personal-info">
-                    
+                    <Link href={"/personal"}>
+                        <Image src={profilPic} width={40} height={40} alt="picture of profile"/>
+                    </Link>
                 </div>
             </nav>
     );
